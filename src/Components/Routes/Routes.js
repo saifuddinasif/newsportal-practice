48bd91c119c52941b1ad6036388fa05c5f5073ab
My Name <mdarfinji45@gmail.com>
2022-10-23 12:26:43 +0800

    setting up route

---
+++ src/Components/Routes/Routes.js
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "../Layout/Main";
+
+
+
+export  const routes = createBrowserRouter([
+
+   {
+    path : '/',
+
+    element: <Main></Main>,
+
+    children :[
+
+        
+    ]
+   }
+
+
+]);
+
---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpgsFhzP8rcPdRTyeziSY2LGD7Yt63w4E",
+  authDomain: "news-portal-practice-bfd97.firebaseapp.com",
+  projectId: "news-portal-practice-bfd97",
+  storageBucket: "news-portal-practice-bfd97.appspot.com",
+  messagingSenderId: "42369402620",
+  appId: "1:42369402620:web:ebbb4a88767ed27b29b979"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
